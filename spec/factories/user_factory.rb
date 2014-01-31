95e28bc0525e38e927c832e6cbf22ee5c6501b2a
aprildroid <aprildroid20@gmail.com>
2014-01-31 12:52:37 +0700

    Associate users to tickets when tickets are created

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do 
 	factory :user do 
 		name "admin"
+		email "sample@example.com"
 		password "password"
 		password_confirmation "password"
 	end
--- spec/features/viewing_tickets_spec.rb
+++ spec/features/viewing_tickets_spec.rb
@@ -5,19 +5,32 @@ feature "Viewing tickets" do
 	before do 
 		textmate_2 = FactoryGirl.create(:project,
 										name: "TextMate 2")
-										
-		FactoryGirl.create(:ticket,
-				project: textmate_2,
-				title: "Make it shiny!",
-				description: "Gradients! Starbursts! Oh my!")
+		
+		user = FactoryGirl.create(:user)
+	    ticket = FactoryGirl.create(:ticket,
+	    			project: textmate_2,
+	    			title: "Make it shiny!",
+	    			description: "Gradients! Starbursts! Oh my!")
+	    ticket.update(user: user)
+						
+# 		FactoryGirl.create(:ticket,
+# 				project: textmate_2,
+# 				title: "Make it shiny!",
+# 				description: "Gradients! Starbursts! Oh my!")
 		
 		internet_explorer = FactoryGirl.create(:project,
 											name: "Internet Explorer")
-											
-	    FactoryGirl.create(:ticket,
-	    			project: internet_explorer,
-	    			title: "Standards compliance",
-	    			description: "Isn't a joke.")
+		ticket_2 = FactoryGirl.create(:ticket,
+					project: internet_explorer,
+					title: "Standards compliance",
+					description: "Isn't a joke.")
+		ticket_2.update(user: user)								
+# 	    FactoryGirl.create(:ticket,
+# 	    			project: internet_explorer,
+# 	    			title: "Standards compliance",
+# 	    			description: "Isn't a joke.")
+	    			
+
 	    			
 	    visit '/'
 	end
